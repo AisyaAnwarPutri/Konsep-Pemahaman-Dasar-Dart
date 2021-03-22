3dd31e82f6a6c0ec70abf7e2e0b187f77dd6a0c2
Aisya <aisyanwarputri@gmail.com>
2021-03-22 15:30:51 +0700

    Menghitung Biaya Parkir

---
+++ menghitung_biaya_parkir/test/menghitung_biaya_parkir_test.dart
@@ -0,0 +1,8 @@
+import 'package:menghitung_biaya_parkir/menghitung_biaya_parkir.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
